d59d537ed5ab53d9bcf0e5798e2bd6f4ff558f10
Mohammed Dawod <stop0@users.noreply.github.com>
2024-07-14 11:24:11 -0400

    Formatted lines of code

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import {NavBar} from "./NavBar"
 import styled from 'styled-components'
-import {Route, Switch} from 'react-router-dom'
 import {ContactForm} from "./contactFrom"
 import Avatar from "../assets/Avatar-Maker.svg"
 import {Cards} from "./Cards"
@@ -43,28 +40,21 @@ export const Home = () => {
 
             <img src ={Avatar}/>
             <h1>Mohammed Dawod</h1>
-            <h4>Software Developer</h4>
             <h5>Based in Philadelphia, PA</h5>
             <h5>dawodmohmmed@gmail.com</h5>
-             {/*<Link to='/Portfolio'>
-                <Button>View Projects</Button>
 
-    </Link>*/}
             
     <section>
         <a href="https://github.com/stop0">
-        <i style={{color:'white', margin:"1rem", fontSize:'48px',marginRight:'5rem'}} class="fa fa-github"></i>
+            <i style={{color:'white', margin:"1rem", fontSize:'48px',marginRight:'5rem'}} class="fa fa-github"></i>
         </a>
-
         <a href="https://www.linkedin.com/in/mohammed-dawod-ba33a5123/">
-
-        <i style={{color:'white', margin:"1rem", fontSize:'48px',marginLeft:'5rem'}} class="fa fa-linkedin"></i>
+        <   i style={{color:'white', margin:"1rem", fontSize:'48px',marginLeft:'5rem'}} class="fa fa-linkedin"></i>
         </a>
     </section>
 
     <Cards/>
     <ContactForm/>
-
         </BackgroundG>
     )
 }
